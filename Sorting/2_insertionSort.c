932a43ecff6f347c4b08302983750255f1181327
Nishant Argade <71985065+NishantArgade@users.noreply.github.com>
2022-11-26 15:06:26 +0530

    add comments

--- Sorting/2_insertionSort.c
+++ Sorting/2_insertionSort.c
@@ -3,21 +3,20 @@
 
 void InsertionSort(int *arr, int n) // take reference(i.e Address) of array for here changes will reflected in main array
 {
-    printf("\nInersion sort running ......\n");
-    int key, j;
-    // Loop for No.passes
-    for (int i = 1; i <= n - 1; i++)
+    printf("\nInersion Sort\n");
+    
+ 
+    for (int i = 1; i <= n - 1; i++)    // Loop for No.passes
     {
-        key = arr[i];
-        j = i - 1;
-
-        // Loop for each passes
-        while (j >= 0 && arr[j] > key)
+        int  temp = arr[i];
+        
+        int  j = i - 1;
+        while (j >= 0 && arr[j] > temp)  // Loop for each passes
         {
             arr[j + 1] = arr[j];
             j--;
         }
-        arr[j + 1] = key;
+        arr[j + 1] = temp;
     }
     printf("\n");
 }
@@ -25,9 +24,8 @@ void InsertionSort(int *arr, int n) // take reference(i.e Address) of array for 
 void dispaly(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
-    {
         printf("%d ", arr[i]);
-    }
+    
 }
 int main()
 {
@@ -36,5 +34,20 @@ int main()
     dispaly(Array, n);       // Before sort
     InsertionSort(Array, n); // to sorted
     dispaly(Array, n);       // after sort
+    
+    /*
+    
+    Algo -> traverse array and compair element with its previous elements & insert that element in its right place
+        
+       S.C -> O(1)
+       Average case T.C -> O(n^2)
+       Best case T.C(already sorted array) -> O(n)
+       Worst case T.C -> O(n^2)
+        
+      Use Case ->
+       1. Adaptable sorting technique
+       2. Stable sorting technique
+           
+   */
     return 0;
 }
