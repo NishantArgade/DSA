7a6f163c0114085cde3029aa02f9e0e3a00e4442
Nishant Argade <71985065+NishantArgade@users.noreply.github.com>
2022-11-26 19:32:02 +0530

    add comments

--- Sorting/5_MergeSort.c
+++ Sorting/5_MergeSort.c
@@ -1,41 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 void Merge(int Arr[], int mid, int low, int high)
 {
-    int i, j, k, tempArr[100];
+    int i, j, k, tempArr[high+1];
     i = low;
     j = mid + 1;
     k = low;
 
+    // Merge two sorted array
     while (i <= mid && j <= high)
     {
         if (Arr[i] < Arr[j])
-        {
-            tempArr[k] = Arr[i];
-            k++, i++;
-        }
+            tempArr[k++] = Arr[i++];
         else
-        {
-            tempArr[k] = Arr[j];
-            k++, j++;
-        }
-        
+            tempArr[k++] = Arr[j++];   
     }
     while (i <= mid)
-    {
-        tempArr[k] = Arr[i];
-        k++, i++;
-    }
+        tempArr[k++] = Arr[i++];
+
     while (j <= high)
-    {
-        tempArr[k] = Arr[j];
-        k++, j++;
-    }
+        tempArr[k++] = Arr[j++];
 
     for (int i = low; i <= high; i++)
-    {
         Arr[i] = tempArr[i];
-    }
+    
 }
 
 void MergeSort(int Arr[], int low, int high)
@@ -54,9 +43,8 @@ void MergeSort(int Arr[], int low, int high)
 void display_Array(int a[], int n)
 {
     for (int i = 0; i < n; i++)
-    {
         printf(" %d ", a[i]);
-    }
+    
     printf("\n");
 }
 int main()
@@ -68,5 +56,20 @@ int main()
     MergeSort(Array, 0, n-1);
     display_Array(Array, n);
 
+    /*
+    Algo -> divide array util get one elements (one ele is always sorted) 
+            Merge two sorted array.
+            recurssion...
+     
+     S.C -> O(n)
+     Average case T.C -> O(n*logn)
+     Best case T.C -> O(n*logn)
+     Worst case T.C -> O(n^2)
+     
+     Application ->
+      1.Inversion problem 
+      2.it is fastest than insertion,bubble,selection;
+    
+    */
     return 0;
 }
